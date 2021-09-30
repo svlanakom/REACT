fa126882beba499f4a6dbd657016e25fcd27aae8
svlanakom <svetakom.222@gmail.com>
2021-09-30 17:17:52 +0300

    lesson1task2

--- lesson1/task1/index.js
+++ lesson1/task1/index.js
@@ -1,4 +1,11 @@
+// <div class="greeting">Hello, React!</div>
+
 const rootElement = document.querySelector("#root");
+// const greetingElem = document.createElement("div");
+// greetingElem.classList.add("greeting");
+// greeting.textContent ="Hello,React";
+
+//rootElement.append(greetingElem);
 
 const greetingElement = React.createElement(
   "div",
