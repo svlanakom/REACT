f82e8e17cd2af85914ac6d2b4521bad2b98c6153
svlanakom <svetakom.222@gmail.com>
2021-10-02 10:20:39 +0300

    lessongg

---
+++ lesson1/les2/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>Search Form</h1>
+    <div>
+      <input type="text" />
+      <button>Search</button>
+    </div>
+  </>,
+  document.getElementById("root")
+);
