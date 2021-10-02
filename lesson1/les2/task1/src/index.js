d33de2b3117f53038d0986fd7359203c930ae44c
svlanakom <svetakom.222@gmail.com>
2021-10-02 11:28:48 +0300

    lesson2task2

--- lesson1/les2/task1/src/index.js
+++ lesson1/les2/task1/src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//should render following html
+
+//<h1>Search Form</h1>
+//<div>
+//    <input type="text" />
+//    <button>Search</button>
+//</div>
+
 ReactDOM.render(
   <>
     <h1>Search Form</h1>
