798de4ad5e6e07973a5237098f86105e8c2a8b36
svlanakom <svetakom.222@gmail.com>
2021-10-05 12:30:14 +0300

    lesson3task

---
+++ lesson1/les3/task3/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Search from "./Search.jsx";
+
+ReactDOM.render(<Search name="Bob" />, document.getElementById("root"));
