d8b00015dc92ac98e3a950ac59d6d69f3d0276be
svlanakom <svetakom.222@gmail.com>
2021-10-06 13:39:28 +0300

    Lesson5task1

---
+++ lesson1/les5/task1/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+import GoodButton from "./GoodButton.jsx";
+
+ReactDOM.render(
+  <>
+    <GoodButton />
+  </>,
+  document.getElementById("root")
+);
