4c157d9e86dcfbf2313dc3c4a9d1807913c523fe
svlanakom <svetakom.222@gmail.com>
2021-10-06 15:06:30 +0300

    lesson5task3

---
+++ lesson1/les5/task3/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Counter from "./Counter.jsx";
+
+ReactDOM.render(
+  <>
+    <Counter />
+  </>,
+  document.getElementById("root")
+);
