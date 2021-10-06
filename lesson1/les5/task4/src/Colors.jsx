1565dec001b91ae366a8b062f069372bf4c8c54a
svlanakom <svetakom.222@gmail.com>
2021-10-06 18:42:51 +0300

    lesson5task4.2

--- lesson1/les5/task4/src/Colors.jsx
+++ lesson1/les5/task4/src/Colors.jsx
@@ -1,42 +1,3 @@
-// import React from "react";
-
-// const RED = "#f00";
-// const GREEN = "#0f0";
-// const BLUE = "#00f";
-
-// class Colors extends React.Component {
-//   setBodyColor = (color) => {
-//     document.body.style.backgroundColor = color;
-//   };
-//   render() {
-//     return (
-//       <div className="colors">
-//         <button
-//           style={{ backgroundColor: RED }}
-//           onClick={this.setBodyColor.bind(this, RED)}
-//           className="colors__button"
-//         >
-//           Click me!
-//         </button>
-//         <button
-//           style={{ backgroundColor: GREEN }}
-//           onClick={() => this.setBodyColor(GREEN)}
-//           className="colors__button"
-//         >
-//           Click ume!
-//         </button>
-//         <button
-//           style={{ backgroundColor: BLUE }}
-//           onClick={() => this.setBodyColor(BLUE)}
-//         >
-//           Click me!
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-// export default Colors;
-
 import React from "react";
 
 const RED = "#f00";
@@ -62,12 +23,11 @@ class Colors extends React.Component {
           onClick={() => this.setBodyColor(GREEN)}
           className="colors__button"
         >
-          Click me!
+          Click ume!
         </button>
         <button
           style={{ backgroundColor: BLUE }}
           onClick={() => this.setBodyColor(BLUE)}
-          className="colors__button"
         >
           Click me!
         </button>
@@ -75,5 +35,45 @@ class Colors extends React.Component {
     );
   }
 }
-
 export default Colors;
+
+// import React from "react";
+
+// const RED = "#f00";
+// const GREEN = "#0f0";
+// const BLUE = "#00f";
+
+// class Colors extends React.Component {
+//   setBodyColor = (color) => {
+//     document.body.style.backgroundColor = color;
+//   };
+//   render() {
+//     return (
+//       <div className="colors">
+//         <button
+//           style={{ backgroundColor: RED }}
+//           onClick={this.setBodyColor.bind(this, RED)}
+//           className="colors__button"
+//         >
+//           Click me!
+//         </button>
+//         <button
+//           style={{ backgroundColor: GREEN }}
+//           onClick={() => this.setBodyColor(GREEN)}
+//           className="colors__button"
+//         >
+//           Click me!
+//         </button>
+//         <button
+//           style={{ backgroundColor: BLUE }}
+//           onClick={() => this.setBodyColor(BLUE)}
+//           className="colors__button"
+//         >
+//           Click me!
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Colors;
