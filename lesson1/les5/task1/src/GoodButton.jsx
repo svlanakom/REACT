54b78e7e04c3aa48ed28f4fc46fbbb4a594550ab
svlanakom <svetakom.222@gmail.com>
2021-10-06 13:47:17 +0300

    Lesson5.111

--- lesson1/les5/task1/src/GoodButton.jsx
+++ lesson1/les5/task1/src/GoodButton.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 
+
+
 const GoodButton = () => {
-  return (
-    <button
-      onClick={() => {
-        alert("Good job!");
-      }}
-      className="fancy-button"
-    >
-      Click me!
-    </button>
-  );
+    return (
+        <button onClick={() => {alert("Good Job!");}} className="fancy-button">Click me!</button>
+    
+    );
+
 };
 
 export default GoodButton;
