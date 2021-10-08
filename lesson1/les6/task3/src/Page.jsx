e8721aaeff455908ab407423e86ed5bab45323aa
svlanakom <svetakom.222@gmail.com>
2021-10-08 17:20:03 +0300

    lesson6task3.1

--- lesson1/les6/task3/src/Page.jsx
+++ lesson1/les6/task3/src/Page.jsx
@@ -1,35 +1,31 @@
 import React from "react";
 import Message from "./Message.jsx";
 
-text1 = "Hello,world!";
-text2 = "Another exciting text.";
+const text1 = "Hello, world!";
+const text2 = "Another exciting text.";
 
 class Page extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: null
-        };
-        setText = text => {
-            this.setState({
-                text
-            });
+  state = { text: null };
 
-    
-        };
-
-    };
+  setText = (text) => {
+    this.setState({ text: text });
+  };
   render() {
     return (
       <div className="page">
-            <Message text={this.state.text} />
-            <div className="actions">
-               <button className="btn" onClick={() => this.setText({ text1 })}>Text 1</button>
-               <button className="btn" onClick={() => this.setText({text2})}>Text 2</button>
-                <button className="btn" onClick={() => this.setText("")}>Clear </button>
-       </div> 
-        
+        <Message text={this.state.text} />
+        <div className="actions">
+          <button className="btn" onClick={() => this.setText(text1)}>
+            Text 1
+          </button>
+          <button className="btn" onClick={() => this.setText(text2)}>
+            Text 2
+          </button>
+          <button className="btn" onClick={() => this.setText("")}>
+            Clear
+          </button>
         </div>
+      </div>
     );
   }
 }
