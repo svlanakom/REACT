ca8231e2ae72737d25da5c06b346369ba68d3e8e
svlanakom <svetakom.222@gmail.com>
2021-10-11 15:24:58 +0300

    lesson7task2

---
+++ lesson1/les7/task2/src/UserList.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import User from "./User.jsx";
+
+class UserList extends React.Component {
+  state = {
+    sorting: null,
+  };
+
+  togglesSorting = () => {
+    const newSorting = this.state.sorting === "asc" ? "desc" : "asc";
+    this.setState({
+      sorting: newSorting,
+    });
+  };
+
+    render() {
+        let usersList;
+    if (this.state.sorting) {
+       usersList = this.props.users.slice().sort((a, b) =>
+        this.state.sorting === "asc" ? a.age - b.age : b.age - a.age
+      );
+    } else {
+        usersList = this.props.users;
+    }
+
+    return (
+      <div>
+        <button className="btn" onClick={this.togglesSorting}>
+          {this.state.sorting || "-"}
+        </button>
+
+        <ul className="users">
+          {usersList.map((user) => (
+            <User key={user.id} name={user.name} age={user.age} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UserList;
