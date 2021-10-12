c39daf629beb97fde8c39bb8b0a6afaafb76b869
svlanakom <svetakom.222@gmail.com>
2021-10-12 16:24:31 +0300

    lesson8task3

---
+++ lesson1/les8/task3/src/Dimensions.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+
+
+class Dimensions extends React.Component {
+    state = {
+        width: null,
+        height: null,
+    };
+    componentDidMount() {
+        window.addEventListener('resize', this.onResize);
+
+        const { innerWidth, innerHeight } = window;
+        this.setDimensions(innerWidth, innerHeight);
+      
+       
+
+
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.onResize)
+    }
+
+    onResize = (e) => {
+        const { innerWidth, innerHeight } = e.target;
+        setDimensions(innerWidth, innerHeight)
+
+      
+    };
+
+    setDimensions = (width, height) => {
+          this.setState({
+            width,
+            height,
+        });
+       document.title = `${innerWidth} x ${innerHeight}`;
+    }
+    
+    render() {
+        return <div className="dimensions">{` ${this.state.width}px - ${this.state.height}px`}</div>;
+    }
+}
+
+// class Dimensions extends React.Component {
+//   state = {
+//     width: null,
+//     height: null,
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('resize', this.onResize);
+//     const { innerWidth, innerHeight } = window;
+//     this.setDimensions(innerWidth, innerHeight);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('resize', this.onResize);
+//   }
+
+//   onResize = e => {
+//     const { innerWidth, innerHeight } = e.target;
+
+//     this.setDimensions(innerWidth, innerHeight);
+//   };
+
+//   setDimensions = (width, height) => {
+//     this.setState({
+//       width,
+//       height,
+//     });
+//     document.title = `${width} x ${height}`;
+//   };
+
+//   render() {
+//     return <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>;
+//   }
+// }
+
+export default Dimensions;
