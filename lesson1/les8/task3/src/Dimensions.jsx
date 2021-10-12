c3eec4d2a423ac938198b532c61761a26ad90427
svlanakom <svetakom.222@gmail.com>
2021-10-12 18:10:32 +0300

    lesson8task3.6

--- lesson1/les8/task3/src/Dimensions.jsx
+++ lesson1/les8/task3/src/Dimensions.jsx
@@ -1,39 +1,37 @@
-import React from "react";
+import React, { Component } from 'react';
 
-class Dimensions extends React.Component {
-  state = {
-    width: null,
-    height: null,
-  };
-  componentDidMount() {
-    window.addEventListener("resize", this.onResize);
+class Dimensions extends Component {
+    state = {
+        width: null,
+        height: null
+    }
 
-    const { innerWidth, innerHeight } = window;
-    this.setDimensions(innerWidth, innerHeight);
-  }
+    componentDidMount() {
+        window.addEventListener('resize', this.onResize);
+        const { innerWidth, innerHeight } = window;
+        this.setDimensions(innerWidth, innerHeight)
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.onResize);
-  }
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.onResize);
+    };
 
-  onResize = (e) => {
-    const { innerWidth, innerHeight } = e.target;
-    setDimensions(innerWidth, innerHeight);
-  };
+    onResize = (e) => {
+        const { innerWidth, innerHeight } = e.target;
+        this.setDimensions(innerWidth, innerHeight)
 
-  setDimensions = (width, height) => {
-    this.setState({
-      width,
-      height,
-    });
-    document.title = `${innerWidth} x ${innerHeight}`;
-  };
+    };
 
-  render() {
-    return (
-      <div className="dimensions">{` ${this.state.width}px - ${this.state.height}px`}</div>
-    );
-  }
+    setDimensions = (width, height) => {
+        this.setState({
+            width,
+            height
+        });
+        document.title = `${width} x ${height}`;
+    }
+    render() {
+        return (<div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>)
+    };
 }
 
 export default Dimensions;
--- lesson1/les8/task3/src/index.js
+++ lesson1/les8/task3/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.scss";
+import "./index.scss";
 import Dimensions from "./Dimensions.jsx";
 
 ReactDOM.render(<Dimensions />, document.getElementById("root"));
