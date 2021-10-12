078af4edc4f5414c6c3a81135660544374add958
svlanakom <svetakom.222@gmail.com>
2021-10-12 14:44:38 +0300

    lesson8task2

---
+++ lesson1/les8/task2/src/Clock.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import moment from "moment";
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+
+
+class Clock extends React.Component {
+  constructor(props) {
+      super(props);
+      
+    this.state = {
+      location: props.location,
+      time: getTimeWithOffset(props.offset).toLocaleTimeString(),
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        time: getTimeWithOffset(this.props.offset).toLocaleTimeString(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__location">{this.props.location}</div>
+        <div className="clock__time">{this.state.time}</div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
