22824393bb77fc169cfc1bc5f269cdc17ff24132
svlanakom <svetakom.222@gmail.com>
2021-10-13 15:04:59 +0300

    Lesson9task5

---
+++ lesson1/les9/task5/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Game from "./Game.jsx";
+
+ReactDOM.render(<Game />, document.getElementById("root"));
---
+++ lesson1/les9/task5/webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  const config = {
+    mode: "development",
+    entry: {
+      main: path.resolve(__dirname, "./src/index.js"),
+    },
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /.\.js$|jsx/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+    devServer: {
+      hot: true,
+    },
+  };
+
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      })
+    );
+  }
+
+  return config;
+};
