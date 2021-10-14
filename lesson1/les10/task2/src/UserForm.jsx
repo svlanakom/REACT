98de364cd391a6bd287be6e10ce1cf772dc7fe78
svlanakom <svetakom.222@gmail.com>
2021-10-14 13:49:33 +0300

    lesson10task2.2

--- lesson1/les10/task2/src/UserForm.jsx
+++ lesson1/les10/task2/src/UserForm.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 
 const UserForm = ({ userData, handleChange }) => {
-    return (
-        <form className="user-form">
-            <input
-                type="text"
-                name="firstname"
-                className="user-form-input"
-                value={userData.firstName}
-                onChange={handleChange}
-            />
-            <input
-                type="text"
-                name="lastname"
-                className="user-form-input"
-                value={userData.lastName}
-                onChange={handleChange}
-            />
-        </form>
-    );
-       
-    
-}
-  
-
+  return (
+    <form className="user-form">
+      <input
+        type="text"
+        name="firstName"
+        className="user-form-input"
+        value={userData.firstName}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="lastName"
+        className="user-form-input"
+        value={userData.lastName}
+        onChange={handleChange}
+      />
+    </form>
+  );
+};
 
 export default UserForm;
