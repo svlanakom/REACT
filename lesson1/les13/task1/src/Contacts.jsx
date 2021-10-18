276f45ead06ddce4514cc7c1ad84456af9bb7cc4
svlanakom <svetakom.222@gmail.com>
2021-10-18 22:01:04 +0300

    lesson13task1

---
+++ lesson1/les13/task1/src/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Products = () => (
+  <div className="page__content">
+    <h1>Contacts</h1>
+    <p>We are here 🗺</p>
+    <Link to="/">Go home</Link>
+  </div>
+);
+
+export default Products;
---
+++ lesson1/les13/task1/src/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Products = () => (
+  <div className="page__content">
+    <h1>Products</h1>
+    <p>We will add some products. Later... 😇</p>
+    <Link to="/contacts">Contacts</Link>
+  </div>
+);
+
+export default Products;
