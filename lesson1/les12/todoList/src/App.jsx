950bd85a412595e2c0688cae31fffb28481e3123
svlanakom <svetakom.222@gmail.com>
2021-10-18 17:28:48 +0300

    lesson12

---
+++ lesson1/les12/todoList/src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import TodoList from './TodoList.jsx';
+
+const App = () => <TodoList />;
+
+export default App;
---
+++ lesson1/les12/todoList/src/tasksGateway.jsx
@@ -0,0 +1,43 @@
+const baseUrl = 'https://61488a44035b3600175b9eb9.mockapi.io/TodoList';
+
+export const createTask = taskData =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+  });
+
+export const fetchTasksList = () =>
+  fetch(baseUrl).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+  });
+
+export const updateTask = (taskId, taskData) =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to updateTask task');
+    }
+  });
+
+export const deleteTask = taskId =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to delete task');
+    }
+  });
