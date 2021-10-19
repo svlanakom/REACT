127815cbb8e55cfc60c25e770e90304d04eea811
svlanakom <svetakom.222@gmail.com>
2021-10-19 11:50:10 +0300

    lesson14task3

---
+++ lesson1/les14/task3/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+import Dimensions from "./Dimensions.jsx";
+
+ReactDOM.render(<Dimensions />, document.getElementById("root"));
