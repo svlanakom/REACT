a2c6f1ba448ba3e9d4d124bf091be6dee57ddd2b
svlanakom <svetakom.222@gmail.com>
2021-11-01 16:58:42 +0200

    lesson2task2

---
+++ lesson1/redux/les2/task2/src/store.js
@@ -0,0 +1,47 @@
+import { createStore } from "redux";
+
+const INCREMENT = "COUNTER/INCREMENT";
+const DECREMENT = "COUNTER/DECREMENT";
+const RESET = "COUNTER/RESET";
+
+export const increment = () => ({
+  type: INCREMENT,
+});
+export const decrement = () => ({
+  type: DECREMENT,
+});
+export const reset = () => ({
+  type: RESET,
+});
+
+const initialState = {
+  history: [],
+};
+
+const counterReducer = (state = initialState, action) => {
+  const { history } = state;
+
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        history: [...history, "+1"],
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        history: history.concat(-1),
+      };
+    case RESET:
+      return {
+        ...state,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
