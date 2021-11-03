4ead3f620631b03cef817192ff27ec7208906ac3
svlanakom <svetakom.222@gmail.com>
2021-11-03 16:15:05 +0200

    lessonreduxles4task2

---
+++ lesson1/redux/les4/task2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+import Users from "./users/Users";
+import store from "./store";
+
+const App = () => (
+  <Provider store={store}>
+    <Users />
+  </Provider>
+);
+
+export default App;
